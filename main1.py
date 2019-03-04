b9187a715dd69120653236477f8ee8006cf9f54e
Mahendra mahet <mahat.mahen@gmail.com>
2019-03-04 20:56:55 +0200

    This is my first commit file

---
+++ main1.py
@@ -0,0 +1,22 @@
+print ("hello world")
+
+# This is python in one video code
+
+'''This is a multiline comment
+
+'''
+sttr= "This is me"
+age= 21
+weight= 75.5
+# numbers strings lists tuples dictionaries
+# Airthmetic operations
+print("The value of 3+5=",3+5)
+print("The value of 3-5=",3-5)
+print("The value of 3*5=",3*5)
+print("The value of 3/5=",3/5)
+print("The value of 3**5=",3**5)
+print("The value of 3//5=",3//5)
+print("This is a \"")
+mls =''' This is a multiline
+string and this will keep going'''
+print(mls)
